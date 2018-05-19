fd23f3cc81513128d8c786c539a692450303bfbb
Bruno Dantas <oliveiradantas96@gmail.com>
2018-05-19 13:18:38 -0300

    Arrumando o flake8

--- hang.py
+++ hang.py
@@ -4,6 +4,7 @@ import sys
 
 WORDLIST_FILENAME = "words.txt"
 
+
 class Word:
     def __init__(self, guesses):
         self.guesses = guesses
@@ -11,20 +12,20 @@ class Word:
         self.lettersGuessed = []
         self.availableLetters = string.ascii_lowercase
 
-    def loadOtherWord (self, wordlist):
+    def loadOtherWord(self, wordlist):
         while True:
             self.secretWord = random.choice(wordlist).lower()
             if self.differentLetters() <= self.guesses:
                 break
 
         return self.secretWord
-        
+
     def loadWords(self):
         """
         Depending on the size of the word list, this function may
         take a while to finish.
         """
-        print"\n"
+        print "\n"
         print "Loading word list from file..."
         try:
             # inFile: file
@@ -48,12 +49,11 @@ class Word:
                     guessed += letter
                 else:
                     guessed += ' _ '
-                    
         return guessed
 
     def isWordGuessed(self):
         for letter in self.secretWord:
-            if not letter in self.lettersGuessed:
+            if letter not in self.lettersGuessed:
                 return False
             else:
                 pass
@@ -61,7 +61,7 @@ class Word:
         return True
 
     def availableWords(self):
-        if self.isWordGuessed() == False and self.guesses > 0:
+        if self.isWordGuessed() is False and self.guesses > 0:
                 return True
         else:
                 return False
@@ -73,38 +73,34 @@ class Word:
                 differentLetters.append(letter)
         return len(differentLetters)
 
-    def startMensseger (self):
+    def startMensseger(self):
         print '\n'
         print '*****************************'
         print 'Welcome to the game, Hangman!'
         print '*****************************'
-        print 'I am thinking of a word that is', len(self.secretWord), ' letters long.'
-        print 'And this word has', self.differentLetters(), ' different letters.'
+        print 'I am thinking of a word that is', len(self.secretWord), 'letters long.'
+        print 'And this word has', self.differentLetters(), 'different letters.'
         print '-------------'
         print'\n'
 
-    def endMenssenger (self):
-        if self.isWordGuessed() == True:
-            print 'Congratulations, you won!'
+    def endMenssenger(self):
+        if self.isWordGuessed() is True:
+            print '*****************************'
+            print '* Congratulations, you won! *'
+            print '*****************************'
         else:
             print 'Sorry, you ran out of guesses.'
             print 'The word was ', self.secretWord, '.'
 
 
-    def validationLetters(self, letter):
-        if letter not in self.availableLetters:
-            print "\n"
-            print("*** Sorry your Input is Wrong ***")
-            print("*** Try to put a letter!!!***")
-            print ("------------")
-
 def hangman():
+
     guesses = 8
     word = Word(guesses)
 
     word.startMensseger()
 
-    while  word.availableWords():
+    while word.availableWords():
 
         print 'You have ', word.guesses, 'guesses left.'
         for letter in word.availableLetters:
@@ -113,9 +109,6 @@ def hangman():
 
         print 'Available letters: ', word.availableLetters
         letter = raw_input('Please guess a letter: ')
-
-        
-
 
         if letter in word.lettersGuessed:
             print 'Oops! You have already guessed that letter: ', word.getGuessedWord()
@@ -127,10 +120,9 @@ def hangman():
         elif letter not in word.availableLetters:
             print "\n"
             print("*** Sorry your Input is Wrong ***")
-            print("*** Try to put a letter!!!***")
-               
+            print("*** Try to put a letter!!! ***")
         else:
-            word.guesses -=1
+            word.guesses -= 1
             word.lettersGuessed.append(letter)
             print 'Oops! That letter is not in my word: ', word.getGuessedWord()
         print '------------'
@@ -138,5 +130,6 @@ def hangman():
 
     else:
         word.endMenssenger()
+
 
 hangman()
